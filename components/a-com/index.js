329ce328b814cd734b99600a48338abbd99191e3
lbc19920615 <1585638808@qq.com>
2021-03-16 10:32:35 +0800

    Update index.js

--- components/a-com/index.js
+++ components/a-com/index.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from "react-native";
 
 function ACom(props) {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        return () => {
+        }
+    });
 
     const handleClick = () => {
         setTimeout(() => {
             setCount(count + 1)
         }, 100);
     };
+
     return (
         <View>
             <Text>{count}</Text>
